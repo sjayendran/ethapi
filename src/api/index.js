3aa009baf6329fc9396c78010e34130ada5fa6f0
skullncode <sandjay@gmail.com>
2018-04-14 23:22:02 +0530

    finished refactoring and finalization of functionality

--- src/api/index.js
+++ src/api/index.js
@@ -1,75 +1,14 @@
-import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
-import {getBalance, createWallet, postTransaction} from '../lib/ethutils';
+//import facets from './facets';
+import wallet from './wallet';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	// mount the facets resource - DEFAULT unused resource that came with the template
+	//api.use('/facets', facets({ config, db }));
 	
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
-
-	//Handle blank parameter case for get balance route
-	api.get('/getBalance/', (req, res) => {
-		res.status(400).json({
-			"status": "error",
-			"error": "Please pass an Ethereum address as a parameter for the Get Balance REST route!"
-		});
-	});
-
-	//Route to get the balance of the supplied Ethereum address
-	api.get('/getBalance/:ethaddr', (req, res) => {
-		console.log('inside get balance route:');
-		console.log(req.params.ethaddr);
-		try{
-			getBalance(req.params.ethaddr, res).then(bal => {
-				res.status(200).json({
-					"status": "ok",
-					"balance": bal 
-				});
-			});
-		}
-		catch(err){
-			res.status(400).json({
-				"status": "error",
-				"error": err.toString()
-			});
-		}
-	});
-
-	//Route to create a new Ethereum wallet with Private Key & Public Address
-	api.get('/createWallet', (req, res) => {
-		let walletDetails = createWallet();
-		res.status(201).json({ 
-			"status": "ok",
-			walletDetails 
-		});
-	});
-
-	//Route to send Ether from one Ethereum source address to another destination address
-	api.post('/transaction', (req, res) => {
-		try{
-			postTransaction(req.body.privateKey, req.body.destination, req.body.amount, res).then(result => 
-				{	if(result !== null){
-						res.status(202).json({
-							"status": "ok",
-							"receipt": result
-						});
-					}
-				}
-			)
-		}
-		catch(err){
-			res.status(400).json({
-				"status": "error",
-				"receipt": err.toString()
-			});
-		}
-	});
+	api.use('/', wallet);
 
 	return api;
 }
--- src/api/wallet.js
+++ src/api/wallet.js
@@ -1,1 +1,69 @@
+import { version } from '../../package.json';
 import { Router } from 'express';
+import {getBalance, createWallet, postTransaction} from '../lib/ethutils';
+
+const walletAPI = Router();
+
+walletAPI.get('/', (req, res) => {
+    res.json({ version });
+});
+
+//Handle blank parameter case for get balance route
+walletAPI.get('/getBalance/', (req, res) => {
+    res.status(400).json({
+        "status": "error",
+        "error": "Please pass an Ethereum address as a parameter for the Get Balance REST route!"
+    });
+});
+
+//Route to get the balance of the supplied Ethereum address
+walletAPI.get('/getBalance/:ethaddr', (req, res) => {
+    console.log('inside get balance route:');
+    console.log(req.params.ethaddr);
+    try{
+        getBalance(req.params.ethaddr, res).then(bal => {
+            res.status(200).json({
+                "status": "ok",
+                "balance": bal 
+            });
+        });
+    }
+    catch(err){
+        res.status(400).json({
+            "status": "error",
+            "error": err.toString()
+        });
+    }
+});
+
+//Route to create a new Ethereum wallet with Private Key & Public Address
+walletAPI.get('/createWallet', (req, res) => {
+    let walletDetails = createWallet();
+    res.status(201).json({ 
+        "status": "ok",
+        walletDetails 
+    });
+});
+
+//Route to send Ether from one Ethereum source address to another destination address
+walletAPI.post('/transaction', (req, res) => {
+    try{
+        postTransaction(req.body.privateKey, req.body.destination, req.body.amount, res).then(result => 
+            {	if(result !== null){
+                    res.status(202).json({
+                        "status": "ok",
+                        "receipt": result
+                    });
+                }
+            }
+        )
+    }
+    catch(err){
+        res.status(400).json({
+            "status": "error",
+            "receipt": err.toString()
+        });
+    }
+});
+
+export default walletAPI;
